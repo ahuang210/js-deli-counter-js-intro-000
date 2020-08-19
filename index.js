1e7007506cf4cc3b973fc591207a5a9f91c888a6
Ada Huang <ada10huang@gmail.com>
2020-08-19 21:38:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ let currentLine = katzDeliLine => {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    let array = [];
+    let array = ["The line is currently: "];
     for (let i = 0; i < katzDeliLine.length; i += 1) {
-      array.push `The line is currently: ${++i}. ${katzDeliLine[--i]}`
+      array.push `${++i}. ${katzDeliLine[--i]}`
     } return array
   }
 }
